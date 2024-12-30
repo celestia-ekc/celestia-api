bbf27725404286faa317e3c4d4526982327a6150
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-12-30 19:46:19 +0530

    folder struture updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return JSONResponse(content={"message":"Welcome to celestia"},status_code=200,media_type="application/json")
