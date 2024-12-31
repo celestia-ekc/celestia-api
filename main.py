0e58a788a31d060fdfa4adf58b23dc866f0cc53a
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-12-31 14:18:48 +0530

    Added  the middleware

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from api.routers.gallery import router as gallery_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+        "http://localhost:5173",  
+        "https://celestia-cse.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,  
+    allow_methods=["GET"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(router=gallery_router, prefix="/gallery", tags=["Gallery"])
 
